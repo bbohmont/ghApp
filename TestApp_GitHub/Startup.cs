5d1f8b97482fe64d191cc128ed951c77e2fb59fb
Raj, Sahana K <sahana.raj@bankofamerica.com>
2018-05-08 11:50:00 -0700

    Add project files.

---
+++ TestApp_GitHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestApp_GitHub.Startup))]
+namespace TestApp_GitHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
